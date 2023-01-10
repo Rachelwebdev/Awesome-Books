f9ad752d8237d040dcd9dad9f4c91a0f131c3931
Ouail-01 <ouaillaarabi@gmail.com>
2023-01-10 10:36:50 +0100

    Fix the linters

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-const formBtn = document.querySelector("#form-button");
-const booksList = document.querySelector(".books-list");
+const formBtn = document.querySelector('#form-button');
+const booksList = document.querySelector('.books-list');
 
 let books = [];
 
 const booksAdd = (id, title, author) => {
-  const li = document.createElement("li");
+  const li = document.createElement('li');
   li.innerHTML = `<h2>${title}</h2>
     <h3>${author}</h3>
     <hr>`;
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Remove";
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Remove';
   li.insertBefore(deleteBtn, li.lastElementChild);
   booksList.appendChild(li);
-  deleteBtn.addEventListener("click", () => {
+  deleteBtn.addEventListener('click', () => {
     books = books.filter((books) => {
       if (books.title !== title && books.author !== author) {
         return true;
-      } else {
-        return false;
       }
+      return false;
     });
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
     li.remove();
   });
 };
 const addBook = (title, author) => {
-  if (title !== "" || author !== "") {
+  if (title !== '' || author !== '') {
     const id = Date.now();
     const bookObj = { id, title, author };
     books.push(bookObj);
-    localStorage.setItem("books", JSON.stringify(books));
-    document.getElementById("title").value = "";
-    document.getElementById("author").value = "";
+    localStorage.setItem('books', JSON.stringify(books));
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
     booksAdd(bookObj.id, bookObj.title, bookObj.author);
   }
 };
-const getBookFromStorage = JSON.parse(localStorage.getItem("books"));
+const getBookFromStorage = JSON.parse(localStorage.getItem('books'));
 if (getBookFromStorage) {
   books = getBookFromStorage;
 }
@@ -43,11 +42,11 @@ books.forEach((book) => {
   booksAdd(book.id, book.title, book.author);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  formBtn.addEventListener("click", (e) => {
+document.addEventListener('DOMContentLoaded', () => {
+  formBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
     addBook(title, author);
   });
 });
