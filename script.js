5b9110dbd9162dc9e31a7c7a2e664a48f28192d6
Ouail-01 <ouaillaarabi@gmail.com>
2023-01-11 09:41:57 +0100

    Add the removeBook

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ class Book {
     this.title = title;
     this.author = author;
   }
+
   addBook() {
     const { title, author } = this;
     const bookObj = { title, author };
@@ -17,5 +18,16 @@ class Book {
       document.getElementById("title").value = "";
       document.getElementById("author").value = "";
     }
+  }
+
+  removeBook() {
+    const { id } = this;
+    books = books.filter((book) => {
+      if (book.id !== id) {
+        return true;
+      }
+      return false;
+    });
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
