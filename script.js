8c2ea093b5b2ab9eb26f3763e844ae75de9753e1
Rachel Isaac <rachelisaac995@gmail.com>
2023-01-11 12:38:27 +0400

    Create addBook method with javascript classes

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const formBtn = document.querySelector('#form-button');
-const booksList = document.querySelector('.books-list');
-let books = JSON.parse(localStorage.getItem('books'));
+const formBtn = document.querySelector("#form-button");
+const booksList = document.querySelector(".books-list");
+let books = JSON.parse(localStorage.getItem("books"));
 
 class Book {
   constructor(id, title, author) {
     this.id = id;
     this.title = title;
     this.author = author;
+  }
+  addBook() {
+    const { title, author } = this;
+    const bookObj = { title, author };
+    if (title !== "" || author !== "") {
+      books.push(bookObj);
+      localStorage.setItem("books", JSON.stringify(books));
+      document.getElementById("title").value = "";
+      document.getElementById("author").value = "";
+    }
   }
 }
