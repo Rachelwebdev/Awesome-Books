f36c23dc554dabb3db206f4824a155ebd0623b02
Ouail-01 <ouaillaarabi@gmail.com>
2023-01-11 09:29:58 +0100

    Add the class book

--- script.js
+++ script.js
@@ -1,52 +1,11 @@
 const formBtn = document.querySelector('#form-button');
 const booksList = document.querySelector('.books-list');
+let books = JSON.parse(localStorage.getItem('books'));
 
-let books = [];
-
-const booksAdd = (id, title, author) => {
-  const li = document.createElement('li');
-  li.innerHTML = `<h2>${title}</h2>
-    <h3>${author}</h3>
-    <hr>`;
-  const deleteBtn = document.createElement('button');
-  deleteBtn.textContent = 'Remove';
-  li.insertBefore(deleteBtn, li.lastElementChild);
-  booksList.appendChild(li);
-  deleteBtn.addEventListener('click', () => {
-    books = books.filter((books) => {
-      if (books.title !== title && books.author !== author) {
-        return true;
-      }
-      return false;
-    });
-    localStorage.setItem('books', JSON.stringify(books));
-    li.remove();
-  });
-};
-const addBook = (title, author) => {
-  if (title !== '' || author !== '') {
-    const id = Date.now();
-    const bookObj = { id, title, author };
-    books.push(bookObj);
-    localStorage.setItem('books', JSON.stringify(books));
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-    booksAdd(bookObj.id, bookObj.title, bookObj.author);
+class Book {
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
   }
-};
-const getBookFromStorage = JSON.parse(localStorage.getItem('books'));
-if (getBookFromStorage) {
-  books = getBookFromStorage;
 }
-books.forEach((book) => {
-  booksAdd(book.id, book.title, book.author);
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  formBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    addBook(title, author);
-  });
-});
